31458a7be5d317aae9107887d047680b0feee936
Dan Cahoon <dpcahoon@gmail.com>
2017-11-13 08:10:39 -0500

    Get rid of service worker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import Root from './containers/Root';
-import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
@@ -17,5 +16,3 @@ if (root) {
 } else {
   throw new Error("Couldn't find react root!");
 }
-
-registerServiceWorker();
